7d1366cf5704cfe93d8a81d673e77ff6e4c6137e
Susheel Jalali <134261059+sjalali12@users.noreply.github.com>
2025-04-20 16:58:42 -0400

    Added C code files for Day1 of W/shop

---
+++ Day-1/signedHighestnum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	long long int max=(long long int)(pow(2,10) -1);
+	long long int min=(long long int)(pow(2,10)* -1);
+	printf("highest number represented by long long int max is %lld\n", max);
+	printf("highest number represented by long long int min is %lld\n", min);
+	return 0;
+}
